49495871047ade163a8dcf4f8729ebec9f8eb612
Eric Jenvey <ejenvey@captechconsulting.com>
2017-03-31 23:56:30 -0400

    Adding templates

---
+++ data_preprocessing_template.R
@@ -0,0 +1,19 @@
+#Data Preprocessing
+
+#Read in data
+dataset <- read.csv("Data.csv")
+# dataset <- dataset[,2:3]
+
+#Partition the data
+library(caTools)
+set.seed(123)
+
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set = subset(dataset, split==TRUE)
+test_set = subset(dataset, split==FALSE)
+
+
+#Scaling the data
+#training_set[,2:3] = scale(training_set[,2:3])
+#test_set[,2:3] = scale(test_set[,2:3])
+
